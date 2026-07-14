007ade2121bd4c1d01ddfc50ce2eff6f01f5aac6
StarBlade <245937422+StarBladeModding@users.noreply.github.com>
2026-07-14 23:38:43 +0000

    f

--- Simulation.js
+++ Simulation.js
@@ -1,13 +1,49 @@
 class Simulation{
   constructor(){
-    
+    this.particles = [];
+    this.AMOUNT_PARTICLES = 100;
+    this.VELOCITY_DAMPING = 1;
+
+    this.instantiateParticles
+  }
+
+  instantiateParticles(){
+
+    let xParticles = Math.sqrt(this.AMOUNT_PARTICLES);
+    let yParticles = xParticles
+
+    for(let x=0; x < xParticles; x++){
+      for(let y=0; y < yParticles; y++){
+        let position = new Vector2(x,y);
+        this.particles.push(new Particle(position));
+      }
+    }
   }
 
   update(dt){
     
   }
 
+  predictPositions(dt){
+    for(let i=0; i < this.particles.length; i++){
+      this.particles[i].prevPosition = this.particles[i].position.Cpy();
+      let positionDelta = Scale(this.particles[i].velocity, dt * this.VELOCITY_DAMPING);
+      this.particles[i].position = Add(this.particles[i].position, )
+    }
+  }
+
+  conmputeNextVelocity(dt){
+    for(let i=0; i < this.particles.length; i++){
+      let velocity = Scale(Sub(this.particles[i].position, this.particles[i].prevPosition), 1.0 / dt);
+      this.particles[i].velocity = velocity;
+    }
+  }
+
   draw(){
-    
+    for(let i=0; i < this.particles.length; i++){
+      let position = this.particles[i].position;
+      let color = this.particles[i].color;
+      DrawUtils.drawPoint(position, 5, color);
+    }
   }
 }
