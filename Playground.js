caea35cf677ea631469cd20d70dbbc518ed0b261
StarBlade <245937422+StarBladeModding@users.noreply.github.com>
2026-07-14 23:40:39 +0000

    werahg

--- Playground.js
+++ Playground.js
@@ -10,11 +10,6 @@ class Playground{
   draw(){
     this.simulation.draw()
 
-    DrawUtils.drawLine(Vector2.Zero(), new Vector2(100,100), "#000000", 10);
-    DrawUtils.drawPoint(new Vector2(100,100), 20, "#0000FF");
-    DrawUtils.strokePoint(new Vector2(100,100), 20, "#FF0000");
-    DrawUtils.drawRect(new Vector2(200,200), new Vector2(100,100), "#00FF00");
-    DrawUtils.drawText(new Vector2(300,300), 20, "#FFFFFF", "Hello World");
   }
 
   onMouseMove(x,y){
