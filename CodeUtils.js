da8190f6c91820a095891f2cf180cba6eb316d65
StarBlade <245937422+StarBladeModding@users.noreply.github.com>
2026-07-14 21:05:19 +0000

    a

--- CodeUtils.js
+++
@@ -1,3 +0,0 @@
-class DrawUtils{
-    static drawPoint
-}
---
+++ DrawUtils.js
@@ -0,0 +1,8 @@
+class DrawUtils{
+    static drawPoint(position, radius, color){
+
+    }
+
+    static strokePoint(position, radius, color){
+        
+    }
